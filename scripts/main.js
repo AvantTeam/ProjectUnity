7efd462972b2517ef2cb8d34f8d2bbb9921a1ef5
younggam <nainjoo2001@gmail.com>
2020-09-01 21:04:39 +0900

    younggam

--- scripts/main.js
+++ scripts/main.js
@@ -1,141 +1,145 @@
 this.global.unity = {};
 
-const loadFile = (prev, array) => {
-	var results = [];
-	var names = [];
+const loadFile = (prev, array) =>
+{
+    var results = [];
+    var names = [];
 
-	var p = prev;
+    var p = prev;
 
-	for(var i = 0; i < array.length; i++){
-		var file = array[i];
+    for (var i = 0; i < array.length; i++)
+    {
+        var file = array[i];
 
-		if(typeof(file) === "object"){
-			p.push(file.name);
-			var temp = loadFile(p, file.childs);
-			results = results.concat(temp.res);
-			names = names.concat(temp.fileNames);
-			p.pop();
-		}else{
-			var temp = p.join("/") + "/" + file;
-			results.push(temp);
-			names.push(file);
-		};
-	};
+        if (typeof(file) === "object")
+        {
+            p.push(file.name);
+            var temp = loadFile(p, file.childs);
+            results = results.concat(temp.res);
+            names = names.concat(temp.fileNames);
+            p.pop();
+        }
+        else
+        {
+            var temp = p.join("/") + "/" + file;
+            results.push(temp);
+            names.push(file);
+        };
+    };
 
-	return {
-		res: results,
-		fileNames: names
-	};
+    return {
+        res: results,
+        fileNames: names
+    };
 };
 
 const script = [
-	{
-		name: "libraries",
-		childs: [
-      {
-        name: "light",
+    {
+        name: "libraries",
         childs: [
-          "light",
-          "lightSource",
-          "lightReflector",
-          "lightReflector",
-          "lightRouter",
-          "lightRepeater"
+            {
+                name: "light",
+                childs: [
+                    "light",
+                    "lightSource",
+                    "lightReflector",
+                    "lightReflector",
+                    "lightRouter",
+                    "lightRepeater"
+                ]
+            },
+            "copterbase",
+            "loader",
+            "chainlaser",
+            "exp",
+            "multiCrafter"
         ]
-      },
-			"copterbase",
-			"loader",
-			"chainlaser",
-			"exp"
-		]
-	},
-
-	{
-		name: "global",
-		childs: [
-			{
-				name: "blocks",
-				childs: [
-					"recursivereconstructor"
-				]
-			},
-
-			{
-				name: "flying-units",
-				childs: [
-					"caelifera",
-					"schistocerca",
-					"anthophila"
-				]
-			},
-
-			{
-				name: "ground-units",
-				childs: [
-					"project-spiboss"
-					//"amphibi-ground"
-				]
-			},
-
-			{
-				name: "naval-units",
-				childs: [
-					"rexed",
-					"storm",
-					"amphibi" //naval version no legs
-				]
-			},
-
-			{
-				name: "turrets",
-				childs: [
-					"burnade",
-					"burnade-test"
-				]
-			}
-		]
-	},
-
-	{
-		name: "imber",
-		childs: [
-			{
-				name: "turrets",
-				childs: [
-					"orb",
-					"shockwire"
-				]
-			}
-		]
-	},
+    },
 
     {
-        name: "koruh",
+        name: "global",
         childs: [
+            {
+                name: "blocks",
+                childs: [
+                    "recursivereconstructor"
+                ]
+            },
+
+            {
+                name: "flying-units",
+                childs: [
+                    "caelifera",
+                    "schistocerca",
+                    "anthophila"
+                ]
+            },
+
+            {
+                name: "ground-units",
+                childs: [
+                    "project-spiboss"
+                    //"amphibi-ground"
+                ]
+            },
+
+            {
+                name: "naval-units",
+                childs: [
+                    "rexed",
+                    "storm",
+                    "amphibi" //naval version no legs
+                ]
+            },
+
             {
                 name: "turrets",
                 childs: [
-                    "laser"
+                    "burnade",
+                    "burnade-test"
                 ]
             }
         ]
     },
 
-	{
-		name: "monolith",
-		childs: [
-			{
-				name: "turrets",
-				childs: [
-					"oracle"
-				]
-			}
-		]
-	}
+    {
+        name: "imber",
+        childs: [
+        {
+            name: "turrets",
+            childs: [
+                "orb",
+                "shockwire"
+            ]
+        }]
+    },
+
+    {
+        name: "koruh",
+        childs: [
+        {
+            name: "turrets",
+            childs: [
+                "laser"
+            ]
+        }]
+    },
+
+    {
+        name: "monolith",
+        childs: [
+        {
+            name: "turrets",
+            childs: [
+                "oracle"
+            ]
+        }]
+    }
 ];
 const loadedScript = loadFile([], script);
-for(var i = 0; i < loadedScript.res.length; i++){
-	var res = loadedScript.res[i];
-	var name = loadedScript.fileNames[i];
-	this.global.unity[name] = require("unity/" + res);
+for (var i = 0; i < loadedScript.res.length; i++)
+{
+    var res = loadedScript.res[i];
+    var name = loadedScript.fileNames[i];
+    this.global.unity[name] = require("unity/" + res);
 };
