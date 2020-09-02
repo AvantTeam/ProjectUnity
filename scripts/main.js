073882325fa233b0f512d1aa473d783a147869be
GlennFolker <glenn.folk08@gmail.com>
2020-09-02 15:30:03 +0700

    who changed this hhh

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,26 @@
 this.global.unity = {};
 
-const loadFile = (prev, array) =>
-{
+const loadFile = (prev, array) =>	{
     var results = [];
     var names = [];
 
     var p = prev;
 
-    for (var i = 0; i < array.length; i++)
-    {
+    for (var i = 0; i < array.length; i++){
         var file = array[i];
 
-        if (typeof(file) === "object")
-        {
+        if (typeof(file) === "object"){
             p.push(file.name);
             var temp = loadFile(p, file.childs);
-            results = results.concat(temp.res);
+            
+			results = results.concat(temp.res);
             names = names.concat(temp.fileNames);
-            p.pop();
-        }
-        else
-        {
+            
+			p.pop();
+        }else{
             var temp = p.join("/") + "/" + file;
-            results.push(temp);
+            
+			results.push(temp);
             names.push(file);
         };
     };
@@ -105,35 +103,38 @@ const script = [
     {
         name: "imber",
         childs: [
-        {
-            name: "turrets",
-            childs: [
-                "orb",
-                "shockwire"
-            ]
-        }]
+			{
+				name: "turrets",
+				childs: [
+					"orb",
+					"shockwire"
+				]
+			}
+		]
     },
 
     {
         name: "koruh",
         childs: [
-        {
-            name: "turrets",
-            childs: [
-                "laser"
-            ]
-        }]
+			{
+				name: "turrets",
+				childs: [
+					"laser"
+				]
+			}
+		]
     },
 
     {
         name: "monolith",
         childs: [
-        {
-            name: "turrets",
-            childs: [
-                "oracle"
-            ]
-        }]
+			{
+				name: "turrets",
+				childs: [
+					"oracle"
+				]
+			}
+		]
     }
 ];
 const loadedScript = loadFile([], script);
