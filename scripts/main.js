340ccf5751315f8a1ef3545501c0b5e9cd55b101
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-09-07 09:38:33 +0900

    haha modName go brrrrrrrrrrr

--- scripts/main.js
+++ scripts/main.js
@@ -167,8 +167,8 @@ for (var i = 0; i < loadedScript.res.length; i++)
 if(!Vars.headless){
   Core.app.post(() => {
     var mod = Vars.mods.locateMod("unity");
-    var change = "mod."+ modName + ".";
-    mod.meta.displayName = Core.bundle.get(change+"name");
-    mod.meta.description = Core.bundle.get(change+"description");
+    var change = "mod."+ "unity" + ".";
+    mod.meta.displayName = Core.bundle.get(change + "name");
+    mod.meta.description = Core.bundle.get(change + "description");
   });
 }
