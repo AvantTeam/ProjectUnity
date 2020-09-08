f37ea23d7d2019533c06d01df369ef36146bc1fc
Xusk <Xusk947@gmail.com>
2020-09-08 13:38:09 +0500

    new T2 amphibi boat

--- scripts/main.js
+++ scripts/main.js
@@ -91,7 +91,9 @@ const script = [
                 childs: [
                     "rexed",
                     "storm",
-                    "amphibi" //naval version no legs
+                    // naval units with no legs
+                    "amphibi",
+                    "craber" 
                 ]
             },
 
