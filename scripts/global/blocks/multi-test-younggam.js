46cb58a6cabdcbe26a2f3b8501d665e66548671f
younggam <nainjoo2001@gmail.com>
2020-09-09 21:43:36 +0900

    bugfix

--- scripts/global/blocks/multi-test-younggam.js
+++ scripts/global/blocks/multi-test-younggam.js
@@ -1,15 +1,12 @@
 const multiLib = require("libraries/multi-lib");
 const multiTest1 = multiLib.MultiCrafter(GenericCrafter, "multi-test-1", [{
         //1  you can skip recipe properties
-        input: {
-
-        },
+        input: {},
         output: {
             power: 5.25
         },
         craftTime: 12
-    },
-    { //2
+    }, { //2
         input: {
             items: ["coal/1", "sand/1"],
             liquids: ["water/5"],
@@ -19,8 +16,7 @@ const multiTest1 = multiLib.MultiCrafter(GenericCrafter, "multi-test-1", [{
             liquids: ["slag/5"],
         },
         craftTime: 60
-    },
-    { //3
+    }, { //3
         input: {
             items: ["pyratite/1", "blast-compound/1"],
             liquids: ["water/5"],
@@ -31,8 +27,7 @@ const multiTest1 = multiLib.MultiCrafter(GenericCrafter, "multi-test-1", [{
             liquids: ["oil/5"],
         },
         craftTime: 72
-    },
-    { //4
+    }, { //4
         input: {
             items: ["sand/1"],
         },
@@ -40,8 +35,7 @@ const multiTest1 = multiLib.MultiCrafter(GenericCrafter, "multi-test-1", [{
             items: ["silicon/1"],
         },
         craftTime: 30
-    },
-    { //5
+    }, { //5
         input: {
             items: ["sand/1", "lead/2"],
             liquids: ["water/5"],
@@ -50,8 +44,7 @@ const multiTest1 = multiLib.MultiCrafter(GenericCrafter, "multi-test-1", [{
             items: ["unity-contagium/1"],
         },
         craftTime: 12
-    },
-    { //6
+    }, { //6
         input: {
             items: ["coal/1", "sand/1"],
             liquids: ["water/5"],
@@ -62,8 +55,7 @@ const multiTest1 = multiLib.MultiCrafter(GenericCrafter, "multi-test-1", [{
             liquids: ["slag/5"],
         },
         craftTime: 60
-    },
-    { //7
+    }, { //7
         input: {
             items: ["pyratite/1", "blast-compound/1"],
             liquids: ["water/5"],
@@ -74,8 +66,7 @@ const multiTest1 = multiLib.MultiCrafter(GenericCrafter, "multi-test-1", [{
             liquids: ["oil/5"],
         },
         craftTime: 72
-    },
-    { //8
+    }, { //8
         input: {
             items: ["sand/1"],
         },
@@ -83,8 +74,7 @@ const multiTest1 = multiLib.MultiCrafter(GenericCrafter, "multi-test-1", [{
             items: ["silicon/1"],
         },
         craftTime: 30
-    },
-    { //9
+    }, { //9
         input: {
             items: ["sand/1", "lead/2"],
             liquids: ["water/5"],
@@ -93,8 +83,7 @@ const multiTest1 = multiLib.MultiCrafter(GenericCrafter, "multi-test-1", [{
             items: ["unity-contagium/1"],
         },
         craftTime: 12
-    },
-    { //10
+    }, { //10
         input: {
             items: ["coal/1", "sand/1"],
             liquids: ["water/5"],
@@ -105,61 +94,50 @@ const multiTest1 = multiLib.MultiCrafter(GenericCrafter, "multi-test-1", [{
             liquids: ["slag/5", "oil/5"],
         },
         craftTime: 60
-    }
-], {
-
-},//for entity, they need constructor for extra properties
-function Extra(){
-    this._obj={
-        a:Time.time()
-    };
-    this.getObj=function(){
-        return this._obj;
-    };
-    this.add=function(){
-        this.super$add();
-        print(this._obj.a);
-    }
-});
-multiTest1.dumpToggle=true;
+    }], {}, //for entity, they need constructor for extra properties
+    function Extra() {
+        this._obj = {
+            a: Time.time()
+        };
+        this.getObj = function() {
+            return this._obj;
+        };
+        this.add = function() {
+            this.super$add();
+            print(this._obj.a);
+        }
+    });
+multiTest1.dumpToggle = true;
 const multiTest2 = multiLib.MultiCrafter(GenericCrafter, "multi-test-2", [{ //1  you can skip recipe properties
-        input: {
-            items: ["sand/1", "lead/1"],
-        },
-        craftTime: 12
+    input: {
+        items: ["sand/1", "lead/1"],
     },
-    { //2
-        input: {
-            items: ["coal/1", "sand/1"],
-        },
-        output: {
-            items: ["thorium/1", "surge-alloy/2"],
-            power: 10
-        },
-        craftTime: 60
+    craftTime: 12
+}, { //2
+    input: {
+        items: ["coal/1", "sand/1"],
     },
-    { //3
-        input: {
-            items: ["pyratite/1", "blast-compound/1"],
-        },
-        output: {
-            items: ["scrap/1", "plastanium/2", "spore-pod/2"],
-        },
-        craftTime: 72
+    output: {
+        items: ["thorium/1", "surge-alloy/2"],
+        power: 10
     },
-    { //4
-        input: {
-            items: ["sand/1"],
-            power: 15
-        },
-        output: {
-            items: ["silicon/1"],
-            power: 10
-        },
-        craftTime: 30
-    }
-], {
-
-}, {
-
-});
+    craftTime: 60
+}, { //3
+    input: {
+        items: ["pyratite/1", "blast-compound/1"],
+    },
+    output: {
+        items: ["scrap/1", "plastanium/2", "spore-pod/2"],
+    },
+    craftTime: 72
+}, { //4
+    input: {
+        items: ["sand/1"],
+        power: 15
+    },
+    output: {
+        items: ["silicon/1"],
+        power: 10
+    },
+    craftTime: 30
+}], {}, function Extra() {});
