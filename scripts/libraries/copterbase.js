3e78395e042a9fe08e8a692bc4d0ea06adadcd61
GlennFolker <glenn.folk08@gmail.com>
2020-09-23 11:14:54 +0700

    units outlines (WIP)

--- scripts/libraries/copterbase.js
+++ scripts/libraries/copterbase.js
@@ -1,96 +1,106 @@
 const copterAI = () => extend(FlyingAI, {
-	attack(circleLength){
-		this.moveTo(this.target, this.unit.range() * 0.8);
-		this.unit.lookAt(this.target);
-	}
+    attack(circleLength){
+        this.moveTo(this.target, this.unit.range() * 0.8);
+        this.unit.lookAt(this.target);
+    }
 });
 
 module.exports = {
-	extend(entity, name, obj, obju){
-		if(typeof(obj) === "undefined") obj = {};
-		if(typeof(obju) === "undefined") obju = {};
+    extend(entity, name, obj, obju){
+        if(typeof(obj) === "undefined") obj = {};
+        if(typeof(obju) === "undefined") obju = {};
 
-		obj = Object.assign({
-			rotor: [],
+        obj = Object.assign({
+            rotor: [],
 
-			fallRotateSpeed: 2.5
-		}, obj, {
-			load(){
-				this.super$load();
+            fallRotateSpeed: 2.5
+        }, obj, {
+            load(){
+                this.super$load();
 
-				obj.rotor.forEach(r => {
-					if(typeof(r.rotOffset) === "undefined") r.rotOffset = 0;
+                obj.rotor.forEach(r => {
+                    if(typeof(r.rotOffset) === "undefined") r.rotOffset = 0;
 
-					if(typeof(r.speed) === "undefined") r.speed = 29;
+                    if(typeof(r.speed) === "undefined") r.speed = 29;
 
-					if(typeof(r.x) === "undefined") r.x = 0;
-					if(typeof(r.y) === "undefined") r.y = 0;
+                    if(typeof(r.x) === "undefined") r.x = 0;
+                    if(typeof(r.y) === "undefined") r.y = 0;
 
-					if(typeof(r.scale) === "undefined") r.scale = 1;
+                    if(typeof(r.scale) === "undefined") r.scale = 1;
 
-					if(typeof(r.bladeCount) === "undefined") r.bladeCount = 4;
+                    if(typeof(r.bladeCount) === "undefined") r.bladeCount = 4;
 
-					r.bladeRegion = Core.atlas.find(this.name + "-rotor-blade");
-					r.topRegion = Core.atlas.find(this.name + "-rotor-top");
-				});
+                    r.bladeRegion = Core.atlas.find(this.name + "-rotor-blade");
+                    r.topRegion = Core.atlas.find(this.name + "-rotor-top");
+                    
+                    r.bladeOutlineRegion = Core.atlas.find(this.name + "-rotor-blade-outline");
+                    r.topOutlineRegion = Core.atlas.find(this.name + "-rotor-top-outline");
+                });
 
-				if(typeof(obj.customLoad) === "function") this.customLoad();
-			},
+                if(typeof(obj.customLoad) === "function") this.customLoad();
+            },
 
-			draw(unit){
-				this.super$draw(unit);
+            draw(unit){
+                this.super$draw(unit);
 
-				if(typeof(obj.customDraw) === "function") this.customDraw(unit);
+                if(typeof(obj.customDraw) === "function") this.customDraw(unit);
 
-				Draw.mixcol(Color.white, unit.hitTime / unit.hitDuration);
+                Draw.mixcol(Color.white, unit.hitTime / unit.hitDuration);
 
-				for(var i = 0; i < obj.rotor.length; i++){
-					var r = obj.rotor[i];
+                for(var i = 0; i < obj.rotor.length; i++){
+                    var r = obj.rotor[i];
 
-					var region = r.bladeRegion;
-					var topRegion = r.topRegion;
+                    var region = r.bladeRegion;
+                    var topRegion = r.topRegion;
 
-					var offx = Angles.trnsx(unit.rotation - 90, r.x, r.y);
-					var offy = Angles.trnsy(unit.rotation - 90, r.x, r.y);
+                    var outline = r.bladeOutlineRegion;
+                    var outlineTop = r.topOutlineRegion;
 
-					var w = region.width * r.scale * Draw.scl;
-					var h = region.height * r.scale * Draw.scl;
+                    var offx = Angles.trnsx(unit.rotation - 90, r.x, r.y);
+                    var offy = Angles.trnsy(unit.rotation - 90, r.x, r.y);
 
-					for(var j = 0; j < r.bladeCount; j++){
-						var angle = ((unit.id * 24) + (Time.time() * r.speed) + ((360 / r.bladeCount) * j) + r.rotOffset) % 360;
+                    var w = region.width * r.scale * Draw.scl;
+                    var h = region.height * r.scale * Draw.scl;
 
-						Draw.alpha(Vars.state.isPaused() ? 1 : Time.time() % 2);
-						Draw.rect(region, unit.x + offx, unit.y + offy, w, h, angle);
-					};
+                    for(var j = 0; j < r.bladeCount; j++){
+                        var angle = ((unit.id * 24) + (Time.time() * r.speed) + ((360 / r.bladeCount) * j) + r.rotOffset) % 360;
 
-					Draw.alpha(1);
-					Draw.rect(topRegion, unit.x + offx, unit.y + offy, unit.rotation - 90);
-				};
+                        Draw.alpha(Vars.state.isPaused() ? 1 : Time.time() % 2);
 
-				Draw.mixcol();
-			}
-		});
+                        Draw.rect(outline, unit.x + offx, unit.y + offy, w, h, angle);
+                        Draw.rect(region, unit.x + offx, unit.y + offy, w, h, angle);
+                    };
 
-		var copter = extendContent(UnitType, name, obj);
-		copter.defaultController = copterAI;
+                    Draw.alpha(1);
 
-		obju = Object.assign(obju, {
-			update(){
-				this.super$update();
+                    Draw.rect(outlineTop, unit.x + offx, unit.y + offy, unit.rotation - 90);
+                    Draw.rect(topRegion, unit.x + offx, unit.y + offy, unit.rotation - 90);
+                };
 
-				if(this.dead){
-					this.rotation += obj.fallRotateSpeed * Mathf.signs[this.id % 2];
-				};
+                Draw.mixcol();
+            }
+        });
 
-				if(typeof(obju.customUpdate) === "function") this.customUpdate();
-			}
-		});
+        var copter = extendContent(UnitType, name, obj);
+        copter.defaultController = copterAI;
 
-		copter.constructor = () => {
-			var unit = extend(entity, obju);
-			return unit;
-		};
+        obju = Object.assign(obju, {
+            update(){
+                this.super$update();
 
-		return copter;
-	}
+                if(this.dead){
+                    this.rotation += obj.fallRotateSpeed * Mathf.signs[this.id % 2];
+                };
+
+                if(typeof(obju.customUpdate) === "function") this.customUpdate();
+            }
+        });
+
+        copter.constructor = () => {
+            var unit = extend(entity, obju);
+            return unit;
+        };
+
+        return copter;
+    }
 };
