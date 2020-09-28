7d206030111dcc07a00cd876d8b2723d786f2cb1
younggam <nainjoo2001@gmail.com>
2020-09-28 20:10:55 +0900

    p r e p a r i n g . . .

---
+++ src/main/java/unity/Main.java
@@ -0,0 +1,15 @@
+package unity;
+import static mindustry.Vars.*;
+import mindustry.mod.*;
+import mindustry.type.*;
+import arc.graphics.*;
+import arc.util.*;
+import arc.util.Log.*;
+public class Main extends Mod{
+	public void init() {
+		Log.log(LogLevel.info,"[@]: @","t e s t i n g","init successed");
+	}
+	public void loadContent() {
+		Log.log(LogLevel.info,"[@]: @","t e s t i n g","load successed");
+	}
+}
