ad9d8eb343f7f3cd6d8172e1d961695bab95a15d
younggam <nainjoo2001@gmail.com>
2020-09-28 22:45:44 +0900

    I learned eclipse beautifier

---
+++ src/main/java/unity/libraries/MultiCrafter.java
@@ -0,0 +1,15 @@
+package unity.libraries;
+
+import mindustry.content.*;
+import mindustry.entities.*;
+import mindustry.gen.*;
+import mindustry.type.*;
+import mindustry.world.blocks.production.GenericCrafter;
+import unity.libraries.Recipe;
+public class MultiCrafter extends GenericCrafter{
+	public final Recipe[] recs;
+	public MultiCrafter(String name,Recipe[] recs) {
+		super(name);
+		this.recs=recs;
+	}
+}
