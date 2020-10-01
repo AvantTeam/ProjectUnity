6b6f38918f071dfa1c7a72e8f7862963aef93f55
younggam <nainjoo2001@gmail.com>
2020-10-01 14:12:52 +0900

    adding block in techtree is h

--- src/unity/Main.java
+++ src/unity/Main.java
@@ -1,22 +1,25 @@
 package unity;
 
+import arc.func.*;
 import arc.graphics.*;
 import arc.util.*;
 import arc.util.Log.*;
 import mindustry.mod.*;
 import mindustry.content.*;
+import mindustry.content.TechTree.*;
 import mindustry.ctype.*;
 import mindustry.type.*;
+import mindustry.world.*;
 import unity.content.*;
 
 import static mindustry.Vars.*;
 
 public class Main extends Mod{
-	private ContentList[] content = {
+	private ContentList[] unityContent = {
 		new UnityItems(),
 		new UnityBlocks()
 	};
-	
+
 	@Override
 	public void init(){
 		enableConsole = true;
@@ -25,9 +28,21 @@ public class Main extends Mod{
 
 	@Override
 	public void loadContent(){
-		for (ContentList list : content){
+		for (ContentList list : unityContent){
 			list.load();
 		}
+		addResearch("graphite-press", UnityBlocks.multiTest1);
 		//Log.log(LogLevel.info, "[@]: @", UnityBlocks.multiTest1.name, String.valueOf(UnityBlocks.multiTest1.getRecipe().output.items.length));
+	}
+
+	private void addResearch(String parentName, Block target){
+		//TODO find more neat way to add block in techTree | candidates:json,loop
+		Block parent = content.getByName(ContentType.block, parentName);
+		TechNode baseNode = TechTree.all.contains(t -> t.content == target)
+			? TechTree.all.find(t -> t.content == target)
+			: TechTree.create(parent, target);
+		TechNode parnode = TechTree.all.find(t -> t.content == parent);
+		if (!parnode.children.contains(baseNode)) parnode.children.add(baseNode);
+		baseNode.parent = parnode;
 	}
 }
