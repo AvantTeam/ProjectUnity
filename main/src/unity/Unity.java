85501d7d07a48bb1caa9bfe5be04e4f8822f8f48
GlennFolker <glenn.folk08@gmail.com>
2020-10-21 16:52:54 +0700

    ¯\\_(.\_.)\_/¯

--- main/src/unity/Unity.java
+++ main/src/unity/Unity.java
@@ -1,12 +1,18 @@
 package unity;
 
+import arc.*;
+import arc.func.*;
+import arc.scene.ui.layout.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.*;
 import mindustry.mod.*;
 import mindustry.mod.Mods.*;
+import mindustry.ui.dialogs.*;
 import mindustry.ctype.*;
+import mindustry.game.EventType.*;
+import mindustry.graphics.*;
 import unity.content.*;
-
-import static arc.Core.*;
-import static mindustry.Vars.*;
 
 public class Unity extends Mod{
 	private final ContentList[] unityContent = {
@@ -19,14 +25,63 @@ public class Unity extends Mod{
 		new UnityTechTree(),
 	};
 
+	public final Seq<String> authors;
+
+	public Unity(){
+		authors = Seq.with(Vars.tree.get("authors").readString("UTF-8").split("\n"));
+
+		Events.on(ClientLoadEvent.class, e -> {
+			Time.runTask(10f, () -> {
+				LoadedMod mod = Vars.mods.locateMod("unity");
+				String pref = "mod.";
+
+				Func<String, String> stringf = value -> Core.bundle.get(pref + value);
+
+				BaseDialog dialog = new BaseDialog(stringf.get("welcome-title"));
+				dialog.addCloseButton();
+				dialog.cont.add("Project Unity v" + mod.meta.version).fillX().wrap().get().setAlignment(Align.center);
+				dialog.cont.row();
+				dialog.cont.add(stringf.get("welcome-text"));
+				dialog.cont.row();
+
+				dialog.cont.add(stringf.get("credits")).fillX().wrap().get().setAlignment(Align.center);
+				dialog.cont.row();
+
+				if(!authors.isEmpty()){
+					dialog.cont.image().color(Pal.accent).fillX().height(3f).pad(3f);
+					dialog.cont.row();
+					dialog.cont.add(stringf.get("authors"));
+					dialog.cont.row();
+
+					dialog.cont.pane(new Table(){{
+						int i = 0;
+						left();
+
+						for(String c : authors){
+							add(c).left().pad(3f).padLeft(6f).padRight(6f);
+
+                    		if(i++ % 3 == 0){
+                        		row();
+                    		}
+						}
+					}});
+
+					dialog.show();
+				}
+			});
+		});
+	}
+
 	@Override
 	public void init(){
-		enableConsole = true;
-		if(!headless){
-			LoadedMod mod = mods.locateMod("unity");
+		Vars.enableConsole = true;
+
+		if(!Vars.headless){
+			LoadedMod mod = Vars.mods.locateMod("unity");
 			String change = "mod." + mod.meta.name + ".";
-			mod.meta.displayName = bundle.get(change + "name");
-			mod.meta.description = bundle.get(change + "description");
+
+			mod.meta.displayName = Core.bundle.get(change + "name");
+			mod.meta.description = Core.bundle.get(change + "description");
 		}
 	}
 
