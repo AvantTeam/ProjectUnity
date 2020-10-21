5dccf1515c32068c38d7f0f301dd3557a16cef22
GlennFolker <glenn.folk08@gmail.com>
2020-10-21 21:54:31 +0700

    fix error

---
+++ main/src/unity/ContributorList.java
@@ -0,0 +1,66 @@
+package unity;
+
+import arc.struct.*;
+
+public final class ContributorList{
+    private static IntMap<Seq<String>> contributors = new IntMap<>(ContributionType.all.length);
+    private static IntMap<Seq<String>> translators = new IntMap<>(Language.all.length);
+
+    static void init(){
+        for(ContributionType type : ContributionType.all){
+            contributors.put(type.ordinal(), new Seq<>());
+        }
+        for(Language lang : Language.all){
+            translators.put(lang.ordinal(), new Seq<>());
+        }
+
+        contributors.get(ContributionType.founder.ordinal()).addAll(
+            "EyeOfDarkness"
+        );
+        contributors.get(ContributionType.collaborator.ordinal()).addAll(
+            "[pink]sk7725",
+            "younggam",
+            "[cyan]GlennFolker",
+            "evl",
+            "Xusk"
+        );
+        contributors.get(ContributionType.contributor.ordinal()).addAll(
+            //noone here yet
+        );
+        contributors.get(ContributionType.translator.ordinal()).addAll(
+            "sk7725",
+            "Xusk"
+        );
+
+        translators.get(Language.korean.ordinal()).addAll(
+            "sk7725"
+        );
+        translators.get(Language.russian.ordinal()).addAll(
+            "Xusk"
+        );
+    }
+
+    public static Seq<String> getBy(ContributionType type){
+        return contributors.get(type.ordinal());
+    }
+
+    public static Seq<String> getBy(Language lang){
+        return translators.get(lang.ordinal());
+    }
+
+    public enum ContributionType{
+        founder,
+        collaborator,
+        contributor,
+        translator;
+
+        public static ContributionType[] all = values();
+    }
+
+    public enum Language{
+        korean,
+        russian;
+
+        public static Language[] all = values();
+    }
+}
--- main/src/unity/Unity.java
+++ main/src/unity/Unity.java
@@ -12,6 +12,7 @@ import mindustry.ui.dialogs.*;
 import mindustry.ctype.*;
 import mindustry.game.EventType.*;
 import mindustry.graphics.*;
+import unity.ContributorList.*;
 import unity.content.*;
 
 public class Unity extends Mod{
@@ -25,21 +26,17 @@ public class Unity extends Mod{
         new UnityTechTree(),
     };
 
-    private Seq<String> authors = new Seq<>();
-
     public Unity(){
+        ContributorList.init();
+
         Events.on(ClientLoadEvent.class, e -> {
-            LoadedMod mod = Vars.mods.locateMod("unity");
-            Vars.tree.addFile("authors", mod.root.child("main").child("assets").child("authors"));
-
-            authors = Seq.with(Vars.tree.get("authors").readString("UTF-8").split("\n"));
-
             Func<String, String> stringf = value -> Core.bundle.get("mod." + value);
 
             Time.runTask(10f, () -> {
                 BaseDialog dialog = new BaseDialog(stringf.get("welcome-title"));
+
                 dialog.addCloseButton();
-                dialog.cont.add("Project Unity v" + mod.meta.version).fillX().wrap().get().setAlignment(Align.center);
+                dialog.cont.add("Project Unity").fillX().wrap().get().setAlignment(Align.center);
                 dialog.cont.row();
                 dialog.cont.add(stringf.get("welcome-text"));
                 dialog.cont.row();
@@ -47,27 +44,61 @@ public class Unity extends Mod{
                 dialog.cont.add(stringf.get("credits")).fillX().wrap().get().setAlignment(Align.center);
                 dialog.cont.row();
 
-                if(!authors.isEmpty()){
-                    dialog.cont.image().color(Pal.accent).fillX().height(3f).pad(3f);
-                    dialog.cont.row();
-                    dialog.cont.add(stringf.get("authors"));
-                    dialog.cont.row();
+                for(ContributionType type : ContributionType.all){
+                    if(type == ContributionType.translator) continue;
 
-                    dialog.cont.pane(new Table(){{
-                        int i = 0;
-                        left();
+                    Seq<String> list = ContributorList.getBy(type);
+                    if(list.size > 0){
+                        dialog.cont.image().color(Pal.accent).fillX().height(3f).pad(3f);
+                        dialog.cont.row();
+                        dialog.cont.add(stringf.get(type.name()));
+                        dialog.cont.row();
 
-                        for(String c : authors){
-                            add(c + "[]").left().pad(3f).padLeft(6f).padRight(6f);
+                        dialog.cont.pane(new Table(){{
+                            int i = 0;
+                            left();
 
-                            if(i++ % 3 == 0){
-                                row();
+                            for(String c : list){
+                                add(c + "[]").left().pad(3f).padLeft(6f).padRight(6f);
+
+                                if(i++ % 3 == 0){
+                                    row();
+                                }
                             }
-                        }
-                    }});
-
-                    dialog.show();
+                        }});
+                    }
                 }
+
+                Seq<String> list = ContributorList.getBy(ContributionType.translator);
+                if(list.size > 0){
+                    for(Language lang : Language.all){
+                        Seq<String> trnsList = ContributorList.getBy(lang);
+                        if(trnsList.size < 1) continue;
+
+                        dialog.cont.image().color(Pal.accent).fillX().height(3f).pad(3f);
+                        dialog.cont.row();
+                        dialog.cont.add(stringf.get("translator"));
+                        dialog.cont.row();
+
+                        dialog.cont.pane(new Table(){{
+                            int i = 0;
+                            left();
+
+                            add(stringf.get("language-" + lang.name())).pad(3f).padLeft(6f).padRight(6f);
+                            for(String c : trnsList){
+                                add(c + "[]");
+
+                                if(i++ % 3 == 0){
+                                    row();
+                                }
+                            }
+
+                            row();
+                        }});
+                    }
+                }
+
+                dialog.show();
             });
         });
     }
@@ -78,17 +109,23 @@ public class Unity extends Mod{
 
         if(!Vars.headless){
             LoadedMod mod = Vars.mods.locateMod("unity");
-            String change = "mod." + mod.meta.name + ".";
+            Func<String, String> stringf = value -> Core.bundle.get("mod." + value);
 
-            mod.meta.displayName = Core.bundle.get(change + "name");
-            mod.meta.description = Core.bundle.get(change + "description");
+            mod.meta.displayName = stringf.get(mod.meta.name + ".displayName");
+            mod.meta.description = stringf.get(mod.meta.name + ".description");
         }
     }
 
     @Override
     public void loadContent(){
         for(ContentList list : unityContent){
-            list.load();
+            try{
+                list.load();
+
+                Log.info("@: Loaded content list: @", getClass().getSimpleName(), list.getClass().getSimpleName());
+            }catch(Exception e){
+                Log.err("Error loading @ content(s): @", getClass().getSimpleName(), e.getMessage());
+            }
         }
     }
 }
