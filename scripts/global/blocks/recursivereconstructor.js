407977456dfa08f3bd59bf795393ced2cc573f23
younggam <nainjoo2001@gmail.com>
2020-11-16 15:49:14 +0900

    nothing ig

--- scripts/global/blocks/recursivereconstructor.js
+++ scripts/global/blocks/recursivereconstructor.js
@@ -58,14 +58,6 @@ const recrect = extendContent(Reconstructor, "recursive-reconstructor", {
 		this.outRegion = Core.atlas.find("unity-factory-out-11");
 		this.inRegion = Core.atlas.find("unity-factory-in-11");
 	},
-
-	icons(){
-		return [
-			Core.atlas.find("unity-recursive-reconstructor"),
-			Core.atlas.find("unity-factory-out-11"),
-			Core.atlas.find("unity-recursive-reconstructor-top")
-		]
-	}
 });
 
 recrect.buildType = () => {
