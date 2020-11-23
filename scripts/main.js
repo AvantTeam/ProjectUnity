128dd32e4978990de4fd69b746442e237db7a670
GlennFolker <glenn.folk08@gmail.com>
2020-11-23 17:22:42 +0700

    ,

--- scripts/main.js
+++ scripts/main.js
@@ -286,7 +286,7 @@ const script = [
                     "pylon",
                     "monument"
                 ]
-            }
+            },
 
             {
                 name: "planets",
