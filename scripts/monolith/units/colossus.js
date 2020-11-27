092b24ad21f20f842536608fbb699c86ff4473bf
GlennFolker <glenn.folk08@gmail.com>
2020-11-27 12:27:51 +0700

    colossus lightning spawn ability - no weapons yet

--- scripts/monolith/units/colossus.js
+++ scripts/monolith/units/colossus.js
@@ -1,9 +1,3 @@
-//represents a class constructor
-const LightningSpawn = () => {
-    this.x = 0;
-    this.y = 0;
-};
-
 const lightningSpawnAbility = extend(Ability, {
     update(unit){
         if(this.timer <= 0){
@@ -11,14 +5,14 @@ const lightningSpawnAbility = extend(Ability, {
                 let lightning = this.lightnings[i];
 
                 if(this.phase > 0){
-                    let u = Units.closestTarget(unit.team(), lightning.x, lightning.y, this.lightningRange);
+                    let u = Units.closestTarget(unit.team, lightning.x, lightning.y, this.lightningRange);
 
                     if(u != null){
                         Tmp.v2.set(u.x, u.y).sub(unit.x, unit.y);
                         let angle = Tmp.v2.angle();
                         let len = Math.min(Math.round(Tmp.v2.len() / 6), Math.round(this.lightningRange / 6));
 
-                        Lightning.create(unit.team(), this.lightningColor, this.lightningDamage, lightning.x, lightning.y, angle, len);
+                        Lightning.create(unit.team, this.lightningColor, this.lightningDamage, lightning.x, lightning.y, angle, len);
                         this.lightningSound.at(lightning.x, lightning.y, Mathf.random(0.8, 1.2));
                     }
                 }
@@ -32,14 +26,11 @@ const lightningSpawnAbility = extend(Ability, {
         //update position
         for(let i = 0; i < this.lightningCount; i++){
             if(typeof(this.lightnings[i]) === "undefined"){
-                this.lightnings[i] = new LightningSpawn();
+                this.lightnings[i] = new Vec2();
             }
 
-            Tmp.v1.trns((360 / i + (Time.time() * this.rotateSpeed * Mathf.signs[unit.id % 2])) % 360, this.lightningOffset * this.phase);
-
             let lightning = this.lightnings[i];
-            lightning.x = unit.x + Tmp.v1.x;
-            lightning.y = unit.y + Tmp.v1.y;
+            lightning.trns((360 * i / this.lightningCount) + (Time.time() * this.rotateSpeed * Mathf.signs[unit.id % 2]), this.lightningOffset * this.phase).add(unit);
         }
 
         this.phase = Mathf.lerpDelta(this.phase, unit.ammof(), this.phaseSpeed);
@@ -49,12 +40,17 @@ const lightningSpawnAbility = extend(Ability, {
     draw(unit){
         for(let i = 0; i < this.lightningCount; i++){
             let lightning = this.lightnings[i];
+            let region = Core.atlas.find("circle-shadow");
+            let r = this.phase * region.width * Draw.scl + this.lightningRadius + (Mathf.sin(Time.time(), 6, 4) * this.phase);
 
-            Draw.color(Pal.lancerLaser, 0.5);
-            Fill.circle(lightning.x, lightning.y, this.phase * 32);
+            Draw.color(this.backColor);
+            Draw.rect(region, lightning.x, lightning.y, r, r);
 
-            Draw.color(Color.white, 0.8);
-            Fill.circle(lightning.x, lightning.y, this.phase * 16);
+            Draw.color(this.frontColor);
+            Draw.rect(region, lightning.x, lightning.y, r / 2, r / 2);
+            
+            Draw.color(Color.white);
+            Draw.rect(region, lightning.x, lightning.y, r / 3, r / 3);
         }
     },
 
@@ -63,17 +59,22 @@ const lightningSpawnAbility = extend(Ability, {
     }
 });
 lightningSpawnAbility.lightnings = [];
-lightningSpawnAbility.timerTarget = 60;
+lightningSpawnAbility.timerTarget = 48;
 lightningSpawnAbility.timer = lightningSpawnAbility.timerTarget;
-lightningSpawnAbility.lightningCount = 12;
-lightningSpawnAbility.lightningRange = 60;
-lightningSpawnAbility.lightningOffset = 128;
+lightningSpawnAbility.lightningCount = 8;
+lightningSpawnAbility.lightningRange = 80;
+lightningSpawnAbility.lightningOffset = 56;
 lightningSpawnAbility.lightningColor = Pal.lancerLaser;
 lightningSpawnAbility.lightningDamage = 100;
 lightningSpawnAbility.lightningSound = Sounds.spark;
-lightningSpawnAbility.rotateSpeed = 4;
+lightningSpawnAbility.lightningRadius = 18;
+lightningSpawnAbility.rotateSpeed = 2;
 lightningSpawnAbility.phase = 0;
 lightningSpawnAbility.phaseSpeed = 0.05;
+lightningSpawnAbility.backColor = Pal.lancerLaser.cpy();
+    lightningSpawnAbility.backColor.a = 0.5;
+lightningSpawnAbility.frontColor = Color.white.cpy();
+    lightningSpawnAbility.frontColor.a = 0.8;
 
 const colossus = extendContent(UnitType, "colossus", {});
 colossus.abilities.add(lightningSpawnAbility);
