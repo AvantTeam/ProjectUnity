95f53e296e48dede2cf950ac6fd66d4bb7869761
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-11-27 21:49:41 +0900

    buff, they were too impractical

--- scripts/koruh/walls.js
+++ scripts/koruh/walls.js
@@ -1,17 +1,17 @@
 const wlib = this.global.unity.limitwall;
 
 const stonewall = wlib.extend(Wall, Wall.WallBuild, "ustone-wall", {
-    maxDamage: 40
-}, {});
-
-const densewall = wlib.extend(Wall, Wall.WallBuild, "dense-wall", {
     maxDamage: 32
 }, {});
 
+const densewall = wlib.extend(Wall, Wall.WallBuild, "dense-wall", {
+    maxDamage: 30
+}, {});
+
 const steelwall = wlib.extend(Wall, Wall.WallBuild, "steel-wall", {
-    maxDamage: 22
+    maxDamage: 20
 }, {});
 
 const steelwallLarge = wlib.extend(Wall, Wall.WallBuild, "steel-wall-large", {
-    maxDamage: 44
+    maxDamage: 40
 }, {});
