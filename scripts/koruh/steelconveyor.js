a8aae1a14b3e7f1e282de440d3ec79714c624036
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-11-28 22:40:06 +0900

    this is real 3.5 conveyor. the real.

--- scripts/koruh/steelconveyor.js
+++ scripts/koruh/steelconveyor.js
@@ -1,1 +1,14 @@
-const steelConveyor = extendContent(Conveyor, "steel-conveyor", {});
+const steelconv = extendContent(Conveyor, "steel-conveyor", {
+    load(){
+        this.super$load();
+        steelconv.realSpeed = steelconv.speed;
+    }
+});
+
+steelconv.buildType = () => extendContent(Conveyor.ConveyorBuild, steelconv, {
+    draw(){
+        steelconv.speed = steelconv.realSpeed / 2;
+        this.super$draw();
+        steelconv.speed = steelconv.realSpeed;
+    }
+})
