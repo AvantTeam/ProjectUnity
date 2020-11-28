0bbe49f8c728778c6b4c94627cd7e9cafb67de45
EyeOfDarkness <64228877+EyeOfDarkness@users.noreply.github.com>
2020-11-28 21:43:31 +0800

    Revert "this is real 3.5 conveyor. the real."
    This reverts commit a8aae1a14b3e7f1e282de440d3ec79714c624036.

--- scripts/koruh/diriumconveyor.js
+++ scripts/koruh/diriumconveyor.js
@@ -1,14 +1,1 @@
-const diriumconv = extendContent(Conveyor, "dirium-conveyor", {
-    load(){
-        this.super$load();
-        diriumconv.realSpeed = diriumconv.speed;
-    }
-});
-
-diriumconv.buildType = () => extendContent(Conveyor.ConveyorBuild, diriumconv, {
-    draw(){
-        diriumconv.speed = diriumconv.realSpeed / 2;
-        this.super$draw();
-        diriumconv.speed = diriumconv.realSpeed;
-    }
-})
+const diriumConveyor = extendContent(Conveyor, "dirium-conveyor", {});
