ba8438b3b47fa378830da11ca3ed0b540054a43e
Xelo <williamchang435@gmail.com>
2020-12-05 22:27:42 +1100

    Loader only uses ClientLoadEvent for some reason.
    Not sure if this works.

--- scripts/libraries/loaders/loader.js
+++ scripts/libraries/loaders/loader.js
@@ -1,17 +1,30 @@
 const toLoad = new Seq();
 
-Events.on(ClientLoadEvent, e => {
-    Core.app.post(() => {
-		if(!toLoad.isEmpty()) toLoad.each(i => {
-			if(typeof(i.init) == "function"){
-               	i.init();
-			}else if(i instanceof ContentList){
-				i.load();
-			};
+if(Vars.headless){
+	Events.on(ServerLoadEvent, e => {
+		Core.app.post(() => {
+			if(!toLoad.isEmpty()) toLoad.each(i => {
+				if(typeof(i.init) == "function"){
+					i.init();
+				}else if(i instanceof ContentList){
+					i.load();
+				};
+			});
 		});
 	});
-});
-
+}else{
+	Events.on(ClientLoadEvent, e => {
+		Core.app.post(() => {
+			if(!toLoad.isEmpty()) toLoad.each(i => {
+				if(typeof(i.init) == "function"){
+					i.init();
+				}else if(i instanceof ContentList){
+					i.load();
+				};
+			});
+		});
+	});
+}
 module.exports = {
 	addInit(list){
 		toLoad.add(list);
