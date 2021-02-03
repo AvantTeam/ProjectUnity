6924d972220a61ba8b38a83e338c76ee7b8d057f
GlennFolker <glenn.folk08@gmail.com>
2021-02-03 21:40:04 +0700

    should fix the error

--- main/src/unity/entities/units/KamiUnit.java
+++ main/src/unity/entities/units/KamiUnit.java
@@ -1,14 +1,20 @@
 package unity.entities.units;
 
-import mindustry.*;
 import mindustry.gen.*;
 import unity.*;
+
+import static mindustry.Vars.*;
 
 public class KamiUnit extends UnitEntity{
     @Override
     public void add(){
         if(!added){
-            Unity.musicHandler.play("kami", () -> !dead && Vars.player.within(this, 500f));
+            Unity.musicHandler.play("kami", () -> 
+                !dead &&
+                player.within(this, 1500f) &&
+                (state.isPlaying() || state.isPaused())
+            );
+
             super.add();
         }
     }
