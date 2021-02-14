08cf592f65ddf83343ff621dc21cf7bc18a53033
GlennFolker <glenn.folk08@gmail.com>
2021-02-14 16:12:56 +0700

    damned anuke

--- main/src/unity/Unity.java
+++ main/src/unity/Unity.java
@@ -96,25 +96,33 @@ public class Unity extends Mod{
         musicHandler.setup();
         antiCheat.setup();
 
-        net.handleClient(UnityInvokePacket.class, packet -> {
-            UnityRemoteReadClient.readPacket(packet.reader(), packet.type);
-        });
+        if(netClient != null){
+            net.handleClient(UnityInvokePacket.class, packet -> {
+                UnityRemoteReadClient.readPacket(packet.reader(), packet.type);
+            });
+        }else{
+            Log.warn("'netClient' is null");
+        }
 
-        net.handleServer(UnityInvokePacket.class, (con, packet) -> {
-            if(con.player == null) return;
-
-            try{
-                UnityRemoteReadServer.readPacket(packet.reader(), packet.type, con.player);
-            }catch(ValidateException e){
-                Log.err("Validation failed for '@': @", e.player, e.getMessage());
-            }catch(RuntimeException e){
-                if(e.getCause() instanceof ValidateException v){
-                    Log.err("Validation failed for '@': @", v.player, v.getMessage());
-                }else{
-                    throw e;
+        if(netServer != null){
+            net.handleServer(UnityInvokePacket.class, (con, packet) -> {
+                if(con.player == null) return;
+    
+                try{
+                    UnityRemoteReadServer.readPacket(packet.reader(), packet.type, con.player);
+                }catch(ValidateException e){
+                    Log.err("Validation failed for '@': @", e.player, e.getMessage());
+                }catch(RuntimeException e){
+                    if(e.getCause() instanceof ValidateException v){
+                        Log.err("Validation failed for '@': @", v.player, v.getMessage());
+                    }else{
+                        throw e;
+                    }
                 }
-            }
-        });
+            });
+        }else{
+            Log.warn("'netServer' is null");
+        }
 
         if(!headless){
             LoadedMod mod = mods.locateMod("unity");
