2825eea476a065580623a9b01d9cdb43c37e0153
EyeOfDarkness <64228877+EyeOfDarkness@users.noreply.github.com>
2021-02-22 00:17:41 +0800

    @264657460327874562

--- main/src/unity/world/blocks/defense/turrets/ObjPowerTurret.java
+++ main/src/unity/world/blocks/defense/turrets/ObjPowerTurret.java
@@ -1,7 +1,9 @@
 package unity.world.blocks.defense.turrets;
 
+import arc.func.*;
 import arc.graphics.g2d.*;
 import arc.math.*;
+import arc.math.geom.*;
 import arc.util.*;
 import mindustry.world.blocks.defense.turrets.*;
 import unity.util.*;
@@ -20,12 +22,39 @@ public class ObjPowerTurret extends PowerTurret{
     }
 
     public class ObjPowerTurretBuild extends PowerTurretBuild{
+        float time = 0f;
+        float distortionTime = 0f;
+
+        @Override
+        public void updateTile(){
+            super.updateTile();
+            if(Float.isNaN(time)) time = 0f;
+            time += efficiency() * (1f + ((reload * 2.5f) / reloadTime)) * Time.delta;
+            distortionTime = Math.max(0f, distortionTime - (Time.delta * 0.2f));
+        }
+
+        @Override
+        public void damage(float damage){
+            distortionTime = Mathf.clamp(Mathf.sqrt(Math.max(0f, damage / 20f)), 0f, 3f);
+            super.damage(damage);
+        }
+
+        protected float getDistortion(){
+            return ((Mathf.clamp(1f - (healthf() * 2f)) * 2f) + distortionTime) / 16f;
+        }
+
         @Override
         public void draw(){
             Draw.rect(baseRegion, x, y);
             Draw.color();
 
-            object.draw(x, y, Mathf.cos(Time.time, 76f, 120f), Mathf.sin(Time.time, 76f, 120f), -rotation);
+            Cons<Vec3> distort = v -> {
+                if(getDistortion() >= 0.001f){
+                    v.add(Mathf.range(getDistortion()), Mathf.range(getDistortion()), Mathf.range(getDistortion()));
+                }
+            };
+
+            object.draw(x, y, Mathf.cos(time, 76f, 120f), Mathf.sin(time, 76f, 120f), -rotation, distort);
             //object.draw(x, y, 0f, 0f, -rotation);
         }
     }
