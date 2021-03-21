7683e6c7ec247bee7e07511db74a55e66698b551
GlennFolker <glenn.folk08@gmail.com>
2021-03-21 16:24:53 +0700

    unit death objective

--- main/src/unity/content/UnitySectorPresets.java
+++ main/src/unity/content/UnitySectorPresets.java
@@ -1,9 +1,12 @@
 package unity.content;
 
+import arc.util.*;
 import mindustry.type.*;
 import mindustry.ctype.*;
 import unity.annotations.Annotations.*;
+import unity.type.*;
 
+import static mindustry.Vars.*;
 import static unity.content.UnityPlanets.*;
 
 public class UnitySectorPresets implements ContentList{
@@ -16,10 +19,19 @@ public class UnitySectorPresets implements ContentList{
             alwaysUnlocked = true;
             captureWave = 15;
         }};*/
-        accretion = new SectorPreset("accretion", megalith, 30){{
+        /*accretion = new SectorPreset("accretion", megalith, 30){{
             alwaysUnlocked = true;
             captureWave = 15;
+        }};*/
+
+        //testing scripted sectors
+        accretion = new ScriptedSector("accretion", megalith, 30){{
+            alwaysUnlocked = true;
+            captureWave = 15;
+
+            objectives.add(new UnitDeathObjective(UnityUnitTypes.kami, 2, () -> {
+                ui.announce("Kami has been defeated!", 2f * Time.toSeconds);
+            }));
         }};
     }
-
 }
--- main/src/unity/type/ScriptedSector.java
+++ main/src/unity/type/ScriptedSector.java
@@ -3,6 +3,7 @@ package unity.type;
 import arc.*;
 import arc.struct.*;
 import mindustry.game.EventType.*;
+import mindustry.gen.*;
 import mindustry.type.*;
 
 import static mindustry.Vars.*;
@@ -10,69 +11,110 @@ import static mindustry.Vars.*;
 public class ScriptedSector extends SectorPreset implements ApplicationListener{
     public Seq<SectorObjective> objectives = new Seq<>();
 
-    public ScriptedSector(String name, Planet planet, int sector, SectorObjective... objectives){
+    public ScriptedSector(String name, Planet planet, int sector){
         super(name, planet, sector);
 
-        this.objectives.addAll(objectives);
         Events.on(SectorLaunchEvent.class, e -> {
             if(e.sector.id == sector) {
                 reset();
                 Core.app.addListener(this);
+                unity.Unity.print("Added into application listeners");
             }
         });
     }
 
     @Override
     public void update(){
-        if(
-            !state.isPlaying() || (
-                state.rules != null
-                ?   (
-                    state.rules.sector != null
-                    ?   state.rules.sector.id == sector.id
-                    :   true
-                )
-                :   true
-            )
-        ){
+        if(!valid()){
             reset();
             Core.app.removeListener(this);
+            unity.Unity.print("Removed from application listeners");
 
             return;
         }
 
         for(SectorObjective objective : objectives){
             objective.update();
-            if(objective.completed() && !objective.executed){
-                objective.listener.execute();
-                objective.executed = true;
+            if(objective.completed() && !objective.isExecuted()){
+                objective.execute();
+                objective.execution++;
             }
         }
     }
 
     public void reset(){
         for(SectorObjective objective : objectives){
-            objective.executed = false;
+            objective.reset();
+            objective.execution = 0;
         }
     }
 
+    public boolean valid(){
+        return state.rules != null
+            ?   (
+                state.rules.sector != null
+                ?   state.rules.sector.id == sector.id
+                :   false
+            )
+            :   false;
+    }
+
     public abstract class SectorObjective{
-        public final ScriptedSector sector;
         public final SectorListener listener;
 
-        private boolean executed = false;
+        protected final int executions;
+        protected int execution;
 
-        public SectorObjective(ScriptedSector sector, SectorListener listener){
-            this.sector = sector;
+        public SectorObjective(int executions, SectorListener listener){
             this.listener = listener;
+            this.executions = executions;
         }
 
         public void update(){}
 
-        public abstract boolean completed();
+        public void reset(){}
+
+        public void execute(){
+            listener.execute();
+        }
 
         public boolean isExecuted(){
-            return executed;
+            return execution >= executions;
+        }
+
+        public abstract boolean completed();
+    }
+
+    public class UnitDeathObjective extends SectorObjective{
+        public final UnitType type;
+        protected int unitId = -1;
+
+        public UnitDeathObjective(UnitType type, int executions, SectorListener listener){
+            super(executions, listener);
+
+            this.type = type;
+            Events.on(UnitCreateEvent.class, e -> {
+                if(!isExecuted() && valid() && unitId == -1 && e.unit.type.id == type.id){
+                    unitId = e.unit.id;
+                }
+            });
+        }
+
+        @Override
+        public void reset(){
+            unitId = -1;
+        }
+
+        @Override
+        public boolean completed(){
+            var unit = Groups.unit.getByID(unitId);
+            return unit != null && unit.dead();
+        }
+
+        @Override
+        public void execute(){
+            super.execute();
+            unitId = -1;
         }
     }
 
