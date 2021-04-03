c737e09c4ad58f0e51b1e615773b046450bb95f5
EyeOfDarkness <64228877+EyeOfDarkness@users.noreply.github.com>
2021-04-03 09:42:18 +0800

    still wip

--- main/src/unity/util/CyclicCoordinateDescent.java
+++ main/src/unity/util/CyclicCoordinateDescent.java
@@ -1,65 +1,60 @@
 package unity.util;
 
 import arc.math.*;
-import arc.math.geom.*;
 import arc.struct.*;
+import arc.util.*;
 
 /**
  * Cyclic Coordinate Descent.
  * Original code by Ryan Juckett
  * http://www.ryanjuckett.com/
  *
- * Ported in java
- * Uses degrees instead of radians*/
+ * Ported in java*/
 public class CyclicCoordinateDescent{
     static final float epsilon = 0.0001f;
 
     static float simplifyAngle(float angle){
-        return Mathf.mod(angle, 360f);
+        angle = angle % 360f;
+        if(angle < -180f){
+            angle += 360f;
+        }else if(angle > 180f){
+            angle -= 360f;
+        }
+        return angle;
     }
 
-    public static void calculate(Bone[] bones, float targetX, float targetY, float worldX, float worldY, float arrivalDist){
-        calculate(bones, targetX, targetY, worldX, worldY, arrivalDist, 361f, 1f, false);
-    }
-
-    public static void calculate(Bone[] bones, float targetX, float targetY, float worldX, float worldY, float arrivalDist, float angleLimit, float angleLerp, boolean delta){
+    public static void calculate(DefaultBone[] bones, float targetX, float targetY, float arrivalDist, float angleLimit, float angleLerp, boolean delta){
         int numBones = bones.length;
         if(numBones <= 0) return;
-
-        targetX -= worldX;
-        targetY -= worldY;
 
         float arrivalDistSqr = arrivalDist * arrivalDist;
 
         Seq<WorldBone> worldBones = new Seq<>();
 
-        bones[0].sub(worldX, worldY);
         WorldBone root = new WorldBone();
-        root.x = bones[0].getCCDPositionX();
-        root.y = bones[0].getCCDPositionY();
-        root.angle = bones[0].angle();
+        root.x = bones[0].x;
+        root.y = bones[0].y;
+        root.angle = bones[0].angle;
         worldBones.add(root);
 
         //for( int boneIdx = 1; boneIdx < numBones; boneIdx++ )
         for(int i = 1; i < numBones; i++){
             WorldBone prevWorldBone = worldBones.get(i - 1);
-            Bone curLocalBone = bones[i];
-            curLocalBone.sub(worldX, worldY);
+            DefaultBone curLocalBone = bones[i];
 
             WorldBone newWorldBone = new WorldBone();
-            newWorldBone.x = prevWorldBone.x + (prevWorldBone.cosAngle() * curLocalBone.getCCDPositionX()) - (prevWorldBone.sinAngle() * curLocalBone.getCCDPositionY());
-            newWorldBone.y = prevWorldBone.y + (prevWorldBone.sinAngle() * curLocalBone.getCCDPositionX()) + (prevWorldBone.cosAngle() * curLocalBone.getCCDPositionY());
+            newWorldBone.x = prevWorldBone.x + prevWorldBone.cosAngle() * curLocalBone.x - prevWorldBone.sinAngle() * curLocalBone.y;
+            newWorldBone.y = prevWorldBone.y + prevWorldBone.sinAngle() * curLocalBone.x + prevWorldBone.cosAngle() * curLocalBone.y;
             //curLocalBone.setX(newWorldBone.x);
             //curLocalBone.setY(newWorldBone.y);
-            newWorldBone.angle = prevWorldBone.angle + curLocalBone.angle();
-            if(angleLimit < 360f) newWorldBone.angle = Utils.clampedAngle(newWorldBone.angle, prevWorldBone.angle, angleLimit);
+            newWorldBone.angle = (prevWorldBone.angle + curLocalBone.angle);
             worldBones.add(newWorldBone);
         }
 
-        float endX = worldBones.get(worldBones.size - 1).x;
-        float endY = worldBones.get(worldBones.size - 1).y;
+        float endX = worldBones.get(numBones - 1).x;
+        float endY = worldBones.get(numBones - 1).y;
 
-        for(int i = worldBones.size - 2; i >= 0; i--){
+        for(int i = numBones - 2; i >= 0; i--){
             float curToEndX = endX - worldBones.get(i).x;
             float curToEndY = endY - worldBones.get(i).y;
             float curToEndMag = Mathf.sqrt(curToEndX * curToEndX + curToEndY * curToEndY);
@@ -81,50 +76,30 @@ public class CyclicCoordinateDescent{
 
             float rotAng = (float)Math.acos(Mathf.clamp(cosRotAng, -1f, 1f));
             if(sinRotAng < 0f) rotAng = -rotAng;
+            rotAng *= Mathf.radDeg;
 
-            endX = worldBones.get(i).x + cosRotAng * curToEndX - sinRotAng * curToEndY;
-            endY = worldBones.get(i).y + sinRotAng * curToEndX + cosRotAng * curToEndY;
+            endX = worldBones.get(i).x + (cosRotAng * curToEndX) - (sinRotAng * curToEndY);
+            endY = worldBones.get(i).y + (sinRotAng * curToEndX) + (cosRotAng * curToEndY);
 
-            float offAngle = angleLerp >= 1f ? simplifyAngle(bones[i].angle() + (rotAng * Mathf.radDeg)) : (!delta ? Mathf.slerp(bones[i].angle(), simplifyAngle(bones[i].angle() + (rotAng * Mathf.radDeg)), angleLerp) : Mathf.slerpDelta(bones[i].angle(), simplifyAngle(bones[i].angle() + (rotAng * Mathf.radDeg)), angleLerp));
+            //float offAngle = angleLerp >= 1f ? simplifyAngle(bones[i].angle() + (rotAng * Mathf.radDeg)) : (!delta ? Mathf.slerp(bones[i].angle(), simplifyAngle(bones[i].angle() + (rotAng * Mathf.radDeg)), angleLerp) : Mathf.slerpDelta(bones[i].angle(), simplifyAngle(bones[i].angle() + (rotAng * Mathf.radDeg)), angleLerp));
+            float lerpAngle = Mathf.slerp(simplifyAngle(bones[i].angle), simplifyAngle(bones[i].angle + rotAng), Mathf.clamp(angleLerp * (delta ? Time.delta : 1f)));
+            float offAngle = angleLerp >= 1f ? simplifyAngle(bones[i].angle + rotAng) : lerpAngle;
+
             //bones[i].angle(simplifyAngle(bones[i].angle() + (rotAng * Mathf.radDeg)));
-            if(angleLimit < 360f) offAngle = Utils.clampedAngle(offAngle, bones[i + 1].angle(), angleLimit);
-            bones[i].angle(offAngle);
-            bones[i].add(worldX, worldY);
+            if(angleLimit < 360f) offAngle = Utils.clampedAngle(offAngle, bones[i + 1].angle, angleLimit);
+            //bones[i].angle(offAngle);
+            bones[i].angle = offAngle;
 
             float endToTargetX = (targetX - endX);
             float endToTargetY = (targetY - endY);
-            if((endToTargetX * endToTargetX + endToTargetY * endToTargetY <= arrivalDistSqr)){
-                //bones[bones.length - 1].add(worldX, worldY);
+            if((endToTargetX * endToTargetX) + (endToTargetY * endToTargetY) <= arrivalDistSqr){
                 break;
             }
         }
-        bones[bones.length - 1].add(worldX, worldY);
     }
 
-    public interface Bone extends Position{
-        float angle();
-        void angle(float d);
-
-        void setX(float x);
-        void setY(float y);
-
-        default float getCCDPositionX(){
-            return getX();
-        }
-
-        default float getCCDPositionY(){
-            return getY();
-        }
-
-        default void add(float x, float y){
-            setX(getCCDPositionX() + x);
-            setY(getCCDPositionY() + y);
-        }
-
-        default void sub(float x, float y){
-            setX(getCCDPositionX() - x);
-            setY(getCCDPositionY() - y);
-        }
+    public static class DefaultBone{
+        public float angle, x, y;
     }
 
     static class WorldBone{
