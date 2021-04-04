bea10bcc5a06f72dcdb8c0189fb994a1f7879ea0
EyeOfDarkness <64228877+EyeOfDarkness@users.noreply.github.com>
2021-04-05 04:01:12 +0800

    Update Tentacle.java

--- main/src/unity/entities/Tentacle.java
+++ main/src/unity/entities/Tentacle.java
@@ -33,7 +33,8 @@ public class Tentacle{
             float dst = last().dst(Tmp.v2) / 200f;
             Tmp.v1.set(last()).approachDelta(Tmp.v2, Math.min(speed, dst)).sub(last());
             endVelocity.add(Tmp.v1).limit(type.speed);
-            last().rotation = Angles.moveToward(last().rotation, last().angleTo(targetPos) + 180f, type.rotationSpeed);
+            last().rotation = Angles.moveToward(last().rotation, last().angleTo(targetPos) + 180f, type.rotationSpeed * Time.delta);
+            last().rotation = Mathf.slerpDelta(last().rotation, last().angleTo(targetPos) + 180f, (endVelocity.len2() / (type.speed * type.speed)) * 0.25f);
             last().pos.add(endVelocity, Time.delta);
         }
 
