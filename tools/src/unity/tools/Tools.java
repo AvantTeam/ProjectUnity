f693fc02f218eb843da338cabb6f374be9e9cfd7
GlennFolker <glenn.folk08@gmail.com>
2021-08-05 17:12:19 +0700

    rearchiving without recompiling

--- tools/src/unity/tools/Tools.java
+++ tools/src/unity/tools/Tools.java
@@ -1,9 +1,27 @@
 package unity.tools;
 
+import arc.files.*;
+import arc.struct.*;
+import arc.util.*;
 import unity.*;
 
 public class Tools{
     public static void main(String[] args){
         Unity.print("Hello world!");
+    }
+
+    public static void command(Seq<String> list, String... commands){
+        list.clear().addAll(commands);
+        if(OS.isWindows){
+            list.insert(0, "cmd");
+            list.insert(1, "/c");
+        }
+    }
+
+    public static void clear(Fi file){
+        file.mkdirs();
+        for(var child : file.list()){
+            child.delete();
+        }
     }
 }
