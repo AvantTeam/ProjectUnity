3b1dfadfc51ade6dd895dfcb2088b10c797622b7
GlennFolker <glenn.folk08@gmail.com>
2021-08-05 19:55:39 +0700

    just update the assets, with exclude strategy

--- tools/src/unity/tools/Tools.java
+++ tools/src/unity/tools/Tools.java
@@ -1,27 +1,10 @@
 package unity.tools;
 
-import arc.files.*;
-import arc.struct.*;
-import arc.util.*;
 import unity.*;
 
+/** Main entry point of the tools module. This must only affect the main project's asset directory. */
 public class Tools{
     public static void main(String[] args){
         Unity.print("Hello world!");
-    }
-
-    public static void command(Seq<String> list, String... commands){
-        list.clear().addAll(commands);
-        if(OS.isWindows){
-            list.insert(0, "cmd");
-            list.insert(1, "/c");
-        }
-    }
-
-    public static void clear(Fi file){
-        file.mkdirs();
-        for(var child : file.list()){
-            child.delete();
-        }
     }
 }
