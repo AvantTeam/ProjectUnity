0ad13d3dd86ae0f6062a3291d0f0d7d8b8a65222
GlennFolker <glenn.folk08@gmail.com>
2021-08-16 10:04:48 +0700

    sk shader

--- main/src/unity/mod/DevBuildImpl.java
+++ main/src/unity/mod/DevBuildImpl.java
@@ -2,7 +2,6 @@ package unity.mod;
 
 import arc.util.*;
 import arc.util.async.*;
-import unity.*;
 import unity.util.*;
 
 import java.util.*;
@@ -13,29 +12,18 @@ import static mindustry.Vars.*;
 public class DevBuildImpl implements DevBuild{
     @Override
     public void init(){
-        initScripts();
-        initCommandLine();
-    }
-
-    private void initScripts(){
-        Time.mark();
-
         enableConsole = true;
         JSBridge.importDefaults(JSBridge.defaultScope);
 
-        Unity.print(Strings.format("Total time to import unity packages: @ms", Time.elapsed()));
-    }
-
-    private void initCommandLine(){
         if(!headless && !android){
             Threads.daemon("Command-Executor", () -> {
-                Scanner scan = new Scanner(System.in);
+                var scan = new Scanner(System.in);
                 String line;
 
                 while((line = scan.nextLine()) != null){
-                    String[] split = line.split("\\s+");
+                    var split = line.split("\\s+");
                     if(OS.isWindows){
-                        String[] args = new String[split.length + 2];
+                        var args = new String[split.length + 2];
                         args[0] = "cmd";
                         args[1] = "/c";
 
@@ -47,8 +35,6 @@ public class DevBuildImpl implements DevBuild{
                     }
                 }
             });
-
-            Unity.print("Done setting up command line listener");
         }
     }
 }
