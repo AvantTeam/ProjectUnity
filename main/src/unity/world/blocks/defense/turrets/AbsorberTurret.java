a7714b79d75d9ded645ad7b7837d4636833c7f9f
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2021-09-03 16:26:54 +0200

    code fix untested

--- main/src/unity/world/blocks/defense/turrets/AbsorberTurret.java
+++ main/src/unity/world/blocks/defense/turrets/AbsorberTurret.java
@@ -1,6 +1,7 @@
 package unity.world.blocks.defense.turrets;
 
 import arc.*;
+import arc.math.Mathf;
 import arc.math.geom.*;
 import arc.util.*;
 import mindustry.gen.*;
@@ -9,11 +10,17 @@ import mindustry.ui.*;
 import mindustry.world.meta.*;
 
 /** @author GlennFolker */
-public class AbsorberTurret extends GenericTractorBeamTurret<Bullet>{
+public class AbsorberTurret extends GenericTractorBeamTurret<Teamc>{
     public float powerProduction = 2.5f;
     public float resistance = 0.4f;
     public float damageScale = 18f;
+    public float damage = 0f;
+    public float force = 0f;
     public float speedScale = 3.5f;
+
+    public boolean targetBullets = false;
+    public boolean targetUnits = true;
+    public boolean targetBuildings = false;
 
     public AbsorberTurret(String name){
         super(name);
@@ -41,34 +48,95 @@ public class AbsorberTurret extends GenericTractorBeamTurret<Bullet>{
     public class AbsorberTurretBuild extends GenericTractorBeamTurretBuild{
         @Override
         protected void findTarget(){
-            target = Groups.bullet
-                .intersect(x - range, y - range, range * 2f, range * 2f)
-                .min(b -> b.team != team && b.type().hittable, b -> b.dst2(this));
+            findTarget(x, y, range);
         }
 
         @Override
         protected void findTarget(Vec2 pos){
-            float r = laserWidth / 2f;
-            target = Groups.bullet
-                .intersect(pos.x - r, pos.y - r, r * 2f, r * 2f)
-                .min(b -> b.team != team && b.type().hittable, b -> b.dst2(pos));
+            findTarget(pos.x, pos.y, laserWidth / 2f);
+        }
+
+        protected void findTarget(float x, float y, float r){
+            Teamc tempTarget = null;
+            target = null;
+            float distance = Float.MAX_VALUE;
+
+            if(targetBullets) {
+                tempTarget = Groups.bullet
+                        .intersect(x - r, y - r, r * 2f, r * 2f)
+                        .min(b -> b.team != team && b.type().hittable, b -> b.dst2(x, y));
+
+                if (tempTarget != null) {
+                    target = tempTarget;
+                    distance = Mathf.dst(x, y, tempTarget.x(), tempTarget.y());
+                }
+            }
+
+            if(targetUnits) {
+                tempTarget = Groups.unit
+                        .intersect(x - r, y - r, r * 2f, r * 2f)
+                        .min(b -> b.team != team && !b.dead, b -> b.dst2(x, y));
+
+                if (tempTarget != null) {
+                    float d = Mathf.dst(x, y, tempTarget.x(), tempTarget.y());
+                    if (d < distance) {
+                        distance = d;
+                        target = tempTarget;
+                    }
+                }
+            }
+
+            if(targetBuildings) {
+                tempTarget = Groups.build
+                        .intersect(x - r, y - r, r * 2f, r * 2f)
+                        .min(b -> b.team != team && !b.dead, b -> b.dst2(x, y));
+
+                if (tempTarget != null) {
+                    float d = Mathf.dst(x, y, tempTarget.x(), tempTarget.y());
+                    if (d < distance)target = tempTarget;
+                }
+            }
         }
 
         @Override
         protected void apply(){
-            target.vel.setLength(Math.max(target.vel.len() - resistance * strength, 0f));
-            target.damage = Math.max(target.damage - (resistance / 2f) * strength * Time.delta, 0f);
+            if(target instanceof Bullet bullet) {
+                bullet.vel.setLength(Math.max(bullet.vel.len() - resistance * strength, 0f));
+                bullet.damage = Math.max(bullet.damage - (resistance / 2f) * strength * Time.delta, 0f);
 
-            if(target.vel.isZero(0.01f) || target.damage <= 0f){
-                target.remove();
+                if (bullet.vel.isZero(0.01f) || bullet.damage <= 0f) {
+                    bullet.remove();
+                }
+            }
+
+            if(target instanceof Unit unit && damage > 0f) {
+                unit.damage(damage);
+            }
+
+            if(target instanceof Building building && damage > 0f){
+                building.damage(damage);
             }
         }
 
         @Override
         public float getPowerProduction(){
-            if(target == null || target.type == null) return 0f;
+            if(target == null) return 0f;
 
-            return (target.type.damage / damageScale) * (target.vel.len() / speedScale) * powerProduction;
+            if(target instanceof Bullet bullet) {
+                if(bullet.type == null) return 0f;
+
+                return (bullet.type.damage / damageScale) * (bullet.vel.len() / speedScale) * powerProduction;
+            }
+
+            if(target instanceof Unit unit) {
+                if(unit.type == null) return 0f;
+
+                return (unit.type.dpsEstimate / damageScale) * (unit.vel.len() / speedScale) * powerProduction;
+            }
+
+            //TODO add Building support (why tho)
+
+            return 0f;
         }
     }
 }
