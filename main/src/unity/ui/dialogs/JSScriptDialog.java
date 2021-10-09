e6474948ccd8baebad0176065edcf03d2a3b7b60
GlennFolker <glenn.folk08@gmail.com>
2021-10-09 13:04:48 +0700

    too broken to be kept around

--- main/src/unity/ui/dialogs/JSScriptDialog.java
+++ main/src/unity/ui/dialogs/JSScriptDialog.java
@@ -26,35 +26,6 @@ public class JSScriptDialog extends BaseDialog{
 
         area = cont.area("", UnityStyles.codeArea, str -> listener.get(str.replace("\r", "\n"))).grow().get();
         area.setFocusTraversal(false);
-        area.setTextFieldListener((f, c) -> {
-            if(f instanceof TextArea a && c == '\t'){
-                var text = a.getText();
-                int cur = a.getCursorPosition();
-
-                int start = -1;
-                for(int i = cur; i >= 0; i--){
-                    char t = text.charAt(i);
-                    if(t == '\n' || t == '\t'){
-                        start = i;
-                        break;
-                    }
-                }
-
-                if(start == -1){
-                    a.setText(text.substring(0, cur) + "    " + text.substring(cur));
-                }else{
-                    int length = 4 - ((cur - (start + 1) + 4) % 4);
-
-                    var indent = new StringBuilder();
-                    // Don't use String#repeat as it's only in Java 11+
-                    for(int i = 0; i < length; i++){
-                        indent.append(" ");
-                    }
-
-                    a.setText(text.substring(0, cur) + indent + text.substring(cur));
-                }
-            }
-        });
     }
 
     /**
