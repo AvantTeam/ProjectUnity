a96a393d92466c2846458935c3b34f3c0a45d02a
Goobrr <ggoobbrr@gmail.com>
2021-10-19 11:53:26 +0700

    endDelay and callOnce

--- main/src/unity/map/cinematic/cutscenes/CallbackCutscene.java
+++ main/src/unity/map/cinematic/cutscenes/CallbackCutscene.java
@@ -4,31 +4,43 @@ import arc.util.Time;
 import unity.map.cinematic.Cutscene;
 
 public class CallbackCutscene extends Cutscene {
-    public float delay;
+    public float startDelay;
+    public float endDelay;
     public float duration;
 
     public Runnable callback;
     private boolean called;
+    private boolean callOnce;
 
-    public CallbackCutscene(float delay, float duration, Runnable callback){
-        this.delay = delay;
+    public CallbackCutscene(float startDelay, float endDelay, float duration, boolean callOnce, Runnable callback){
+        this.startDelay = startDelay;
+        this.endDelay = endDelay;
         this.duration = duration;
         this.callback = callback;
+        this.callOnce = callOnce;
+    }
+
+    public CallbackCutscene(float duration, boolean callOnce, Runnable callback){
+        this(0f, 0f, duration, callOnce, callback);
     }
 
     public CallbackCutscene(float duration, Runnable callback){
-        this(0f, duration, callback);
+        this(0f, 0f, duration, true, callback);
+    }
+
+    public CallbackCutscene(Runnable callback){
+        this(0f, 0f, 60f, true, callback);
     }
 
     @Override
     public boolean update() {
         float elapsed = Time.time - startTime();
 
-        if(elapsed >= delay && !called){
+        if(elapsed >= startDelay && elapsed <= startDelay + duration && !called){
             callback.run();
-            called = true;
+            called = callOnce;
         }
 
-        return elapsed >= delay + duration;
+        return elapsed >= startDelay + duration + endDelay;
     }
 }
