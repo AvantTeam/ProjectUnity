b8142cff4d6c4efe654dc4c7f8ed8bacc1088fa4
GlennFolker <glenn.folk08@gmail.com>
2022-02-13 16:11:16 +0700

    hardlock the dumb

--- main/src/unity/ui/UnderworldMap.java
+++ main/src/unity/ui/UnderworldMap.java
@@ -105,19 +105,20 @@ public class UnderworldMap extends Element {
             darknessTexture.dispose();
         }
 
-        pixmap = new Pixmap(world.width() * 32, world.height() * 32);
+        pixmap = wallPixmap = shadowPixmap = darknessPixmap = new Pixmap(1, 1);
+        //pixmap = new Pixmap(world.width() * 32, world.height() * 32);
         texture = new Texture(pixmap);
         region = new TextureRegion(texture);
 
-        wallPixmap = new Pixmap(world.width() * 32, world.height() * 32);
+        //wallPixmap = new Pixmap(world.width() * 32, world.height() * 32);
         wallTexture = new Texture(wallPixmap);
         wallRegion = new TextureRegion(wallTexture);
 
-        shadowPixmap = new Pixmap(world.width(), world.height());
+        //shadowPixmap = new Pixmap(world.width(), world.height());
         shadowTexture = new Texture(shadowPixmap);
         shadowRegion = new TextureRegion(shadowTexture);
 
-        darknessPixmap = new Pixmap(world.width(), world.height());
+        //darknessPixmap = new Pixmap(world.width(), world.height());
         darknessTexture = new Texture(darknessPixmap);
         darknessRegion = new TextureRegion(darknessTexture);
     }
@@ -151,6 +152,7 @@ public class UnderworldMap extends Element {
 
     public static void updateAll(){
         // TODO performance sucks
+        if(true) return;
         for(Tile tile : world.tiles){
             if(tile != null) {
                 if (!(tile.block() != null && tile.block() instanceof StaticWall) && tile.floor() != null && tile.floor().region != null) {
@@ -185,7 +187,7 @@ public class UnderworldMap extends Element {
 
         shadowTexture.setFilter(Texture.TextureFilter.linear, Texture.TextureFilter.linear);
         darknessTexture.setFilter(Texture.TextureFilter.linear, Texture.TextureFilter.linear);
-        
+
         pixmap.dispose();
         wallPixmap.dispose();
         shadowPixmap.dispose();
