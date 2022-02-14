28e17cdc799d3a2daa299fe2929ee40fe7f7db48
GlennFolker <glenn.folk08@gmail.com>
2022-02-14 09:01:38 +0700

    o u t

--- main/fetched/unity/fetched/RotComp.java
+++
@@ -1,9 +0,0 @@
-package unity.fetched;
-
-import unity.annotations.Annotations.*;
-import mindustry.gen.*;
-
-@EntityComponent(write = false)
-abstract class RotComp implements Entityc{
-    @SyncField(false) @SyncLocal float rotation;
-}
