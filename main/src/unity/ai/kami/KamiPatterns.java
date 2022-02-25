fbf73dfd35ee43bc3f651b77fd0a2f5d387f0b54
EyeOfDarkness <64228877+EyeOfDarkness@users.noreply.github.com>
2022-02-25 21:45:53 +0800

    patterns

--- main/src/unity/ai/kami/KamiPatterns.java
+++ main/src/unity/ai/kami/KamiPatterns.java
@@ -87,15 +87,15 @@ public class KamiPatterns{
 
         expandPattern = new StagePattern(-4, bossBasic, new Stage(2.5f * 60f, 2, (ai, data) -> {
             Unit u = ai.unit;
-            if(data.time < 1.75f * 60f && ai.shoot(2, 5f)){
+            if(data.time < 1.75f * 60f && ai.shoot(2, 8f)){
                 int amount = (ai.difficulty * 2) + (int)(ai.reloads[0]) + 1;
                 float spread = 18f / (1f + ai.difficulty / 3f);
                 Angles.shotgun(amount, spread, ai.reloads[1] + Mathf.sin(ai.reloads[3], 3.5f, spread / 1.5f) * ai.reloads[4], f -> {
                     Vec2 v = Tmp.v1.trns(f, 16f);
                     KamiBullet b = (KamiBullet)UnityBullets.kamiBullet3.create(u, u.team, u.x + v.x, u.y + v.y, f);
                     b.width = b.length = 14f;
-                    b.lifetime = 7f * 60f;
-                    b.vel.scl(4.5f + ai.difficulty / 6f);
+                    b.lifetime = 8f * 60f;
+                    b.vel.scl(2.75f + ai.difficulty / 6f);
                 });
 
                 ai.reloads[0] += 1f;
@@ -110,11 +110,11 @@ public class KamiPatterns{
             if(data.time < 120f && ai.shoot(2, 5f)){
                 int amount = ai.difficulty * 4 + (int)(ai.reloads[0]) + 8;
                 for(int i = 0; i < amount; i++){
-                    float ang = (i * (360f / amount)) + ai.reloads[1] + Mathf.sin(ai.reloads[3], 3f, 180f / amount) * ai.reloads[5];
+                    float ang = (i * (360f / amount)) + ai.reloads[1] + Mathf.sin(ai.reloads[3], 4f, 180f / amount) * ai.reloads[5];
                     KamiBullet b = (KamiBullet)UnityBullets.kamiBullet3.create(u, u.team, u.x, u.y, ang);
                     b.width = b.length = 16f;
-                    b.lifetime = 8f * 60f;
-                    b.vel.scl(3f + ai.difficulty / 8f);
+                    b.lifetime = 9f * 60f;
+                    b.vel.scl(2f + ai.difficulty / 8f);
                 }
 
                 ai.reloads[0] += 1f + ai.difficulty * 0.5f;
@@ -128,9 +128,9 @@ public class KamiPatterns{
             }
         };
 
-        flowerPattern = new StagePattern(-3, new Stage(3.5f * 60f, 2, (ai, d) -> {
+        flowerPattern = new StagePattern(-3, new Stage(4.5f * 60f, 2, (ai, d) -> {
             Unit u = ai.unit;
-            if(d.time < 120f && ai.shoot(2, 7f)){
+            if(d.time < 170f && ai.shoot(2, 7f)){
                 int petals = Mathf.clamp(3 + ai.difficulty, 0, 8),
                 amount = 4 + ai.difficulty, len = ai.difficulty;
                 float spacing = ((180f / petals) / (120f / 7f));
@@ -138,7 +138,7 @@ public class KamiPatterns{
                     float ang = i * 360f / petals + ai.reloads[0];
                     int[] sign = ai.reloads[1] <= 0f ? zero : Mathf.signs;
                     for(int s : sign){
-                        shootLine(3f - len / 16f, 6f + len / 4f, amount, v -> {
+                        shootLine(3f - len / 16f, 5f + len / 4f, amount, v -> {
                             KamiBullet b = (KamiBullet)UnityBullets.kamiBullet3.create(u, u.team, u.x, u.y, ang + ai.reloads[1] * s);
                             b.width = b.length = 12f;
                             b.lifetime = 8f * 60f;
