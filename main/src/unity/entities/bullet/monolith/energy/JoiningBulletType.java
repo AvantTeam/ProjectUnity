197de40b5dccaf3b886434e8c6cf9b607f5109a5
GlennFolker <glenn.folk08@gmail.com>
2022-04-04 10:59:00 +0700

    .

--- main/src/unity/entities/bullet/monolith/energy/JoiningBulletType.java
+++ main/src/unity/entities/bullet/monolith/energy/JoiningBulletType.java
@@ -6,6 +6,7 @@ import arc.graphics.g2d.*;
 import arc.math.*;
 import arc.math.geom.*;
 import arc.util.*;
+import mindustry.content.*;
 import mindustry.entities.*;
 import mindustry.entities.bullet.*;
 import mindustry.gen.*;
